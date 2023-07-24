3c1b053daa395ccfe4fa2fc7b67213b5e7a925cf
Ratala Lokesh Sai Kumar <121549200+LokeshRatala@users.noreply.github.com>
2023-07-24 13:53:02 +0530

    Add files via upload
    app

---
+++ App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import About from './components/About';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h3> About Us page</h3>
+      
+      
+      <About/>
+      
+    </div>
+  );
+}
+
+export default App;
